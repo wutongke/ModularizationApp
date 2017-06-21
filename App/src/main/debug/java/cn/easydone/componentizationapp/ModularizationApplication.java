b9023ce8e48f53b122ee843d36460c27fc6ccede
wutongke <ximu470791413@163.com>
2017-06-21 08:10:48 +0800

    fix build app as module

---
+++ App/src/main/debug/java/cn/easydone/componentizationapp/ModularizationApplication.java
@@ -0,0 +1,28 @@
+package cn.easydone.componentizationapp;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.github.mzule.activityrouter.annotation.Modules;
+import com.linked.erfli.library.utils.EventPool;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+/**
+ * Created by erfli on 11/1/16.
+ */
+@Modules({"app"})
+public class ModularizationApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        EventBus.getDefault().register(this);
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void logActivityCreate(EventPool.ActivityNotify activityNotify) {
+        Log.d("ActivityCreate", activityNotify.activityName);
+    }
+}
